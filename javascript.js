c53063964fef96a283fbc0311bef0a375e9f251a
Doku <dokudemo@proton.me>
2026-07-10 13:48:07 -0700

    Add game choices array and random computer selection

---
+++ javascript.js
@@ -0,0 +1,11 @@
+// arr with 3 choises
+let rockPepperScissors = ['rock', 'papper', 'scissors'];
+
+// function take 1 num(max) and return random num(between 0 and max num)
+const getRandomOfNum = maxNum => Math.floor(Math.random() * maxNum);
+
+// function return random element from rockPepperScissors arr
+function getComputerChoice() {
+    return rockPepperScissors[getRandomOfNum(3)];
+}
+
