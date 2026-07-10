6b93783b0e96a4e52468d3b14ffa17c715b7d570
Doku <dokudemo@proton.me>
2026-07-10 14:06:31 -0700

    Remove case sensitivity from getHumanChoice()

--- javascript.js
+++ javascript.js
@@ -1,6 +1,9 @@
 // arr with 3 choises
 let rockPepperScissors = ['rock', 'papper', 'scissors'];
 
+let humanScore = 0;
+let computerScore = 0;
+
 // function take 1 num(max) and return random num(between 0 and max num)
 const getRandomOfNum = maxNum => Math.floor(Math.random() * maxNum);
 
@@ -9,8 +12,7 @@ function getComputerChoice() {
     return rockPepperScissors[getRandomOfNum(3)];
 }
 
+// function return human choise
 function getHumanChoice() {
-    return prompt('Make choise: rock, papper or scissors');
+    return prompt('Make choise: rock, papper or scissors').toLowerCase();
 }
-
-console.log(getHumanChoice());
